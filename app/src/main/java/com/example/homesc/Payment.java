d883ff70a343e9c0cb1cb90e3bc80dfa8763cdc6
flypotwedg <lymarc0808@gmail.com>
2022-04-28 00:49:16 -0500

    think it all works, i go nappy

--- app/src/main/java/com/example/homesc/Payment.java
+++ app/src/main/java/com/example/homesc/Payment.java
@@ -8,13 +8,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class  Payment extends AppCompatActivity {
+public class Payment extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button button = findViewById(R.id.tosucccessful);
+        setContentView(R.layout.activity_payment);
+        Button button = findViewById(R.id.tosuccessful);
         EditText creditNumber=findViewById(R.id.spaceforcardno);
         EditText name=findViewById(R.id.spaceforname);
         EditText expdate=findViewById(R.id.spaceforexpdate);
@@ -24,30 +24,30 @@ public class  Payment extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // startActivity(new Intent(MainActivity.this, successful.class));
-                // Intent intent = new Intent(MainActivity.this, successful.class);
-                //startActivity(intent);
-                //  String number=creditNumber.getText().toString().replaceAll()("[^\\d"]","")
+
                 String number = creditNumber.getText().toString().replaceAll("[^\\d]","");
                 String threeDigit = cvv.getText().toString().replaceAll("[^\\d]","");
                 String exp = expdate.getText().toString().replaceAll("[^\\d]","");
-                //check that credit number is 16 digits, pin is 3 digits, exp date is MM/YYYY
+
                 if(number.length() != 16){
-                    creditNumber.setError("Incorrect information");
+                    creditNumber.setError("Invalid Card Number");
+                    setResult(RESULT_CANCELED);
                 }
                 else if (threeDigit.length() != 3){
-                    cvv.setError("Incorrect information");
+                    cvv.setError("Invalid CVV");
+                    setResult(RESULT_CANCELED);
                 }
                 else if(exp.length() != 6){
-                    expdate.setError("Incorrect information");
+                    expdate.setError("Invalid Expiration");
+                    setResult(RESULT_CANCELED);
                 }
                 else if(zipcode.length()!=5){
-                    zipcode.setError("Incorrect information");
+                    zipcode.setError("Invalid Zipcode");
+                    setResult(RESULT_CANCELED);
                 }
                 else{
-                    startActivity(new Intent(Payment.this, successful.class));
-                    Intent intent = new Intent(Payment.this, successful.class);
-                    startActivity(intent);
+                    setResult(RESULT_OK);
+                    finish();
                 }
 
             }}
@@ -55,9 +55,8 @@ public class  Payment extends AppCompatActivity {
         cancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(Payment.this, UserHomePage.class));
-                Intent intent = new Intent(Payment.this, UserHomePage.class);
-                startActivity(intent);
+                setResult(2);
+                finish();
             }
         } );
 
